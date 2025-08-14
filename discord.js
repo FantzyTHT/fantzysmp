3f0c54392a0217a92dd9242cd62a746fa507c02f
FantzyTHT <robertucciryan@gmail.com>
2025-08-13 21:39:03 -0400

    Update and rename discord.js to discord.html

--- discord.js
+++
@@ -1,8 +0,0 @@
-const embed = new EmbedBuilder();
-embed.setTitle("FantzySMP");
-embed.setURL("https://www.fantzysmp.xyz");
-embed.setDescription("FantzySMP is a growing Minecraft server offering Survival, Creative, and more exciting modes coming soon.");
-embed.setImage("https://www.fantzysmp.xyz/server_banner.png");
-embed.setColor("#ffffff");
-
-await message.reply({ embeds: [embed] });
