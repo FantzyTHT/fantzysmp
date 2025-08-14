8b2b592013d968d101c8a438016dec41e16b13c1
FantzyTHT <robertucciryan@gmail.com>
2025-08-13 20:34:30 -0400

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,11 +1,8 @@
-await message.reply({
-  embeds: [{
-    title: "FantzySMP",
-    url: "https://www.fantzysmp.xyz",
-    description: "FantzySMP is a growing Minecraft server offering Survival, Creative, and more exciting modes coming soon.",
-    image: {
-      url: "https://www.fantzysmp.xyz/server_banner.png"
-    },
-    color: "#ffffff"
-  }]
-});
+const embed = new EmbedBuilder()
+  .setTitle("FantzySMP")
+  .setURL("https://www.fantzysmp.xyz")
+  .setDescription("FantzySMP is a growing Minecraft server offering Survival, Creative, and more exciting modes coming soon.")
+  .setImage("https://www.fantzysmp.xyz/server_banner.png")
+  .setColor("#ffffff");
+
+await message.reply({ embeds: [embed] });
