34dda6bfcfc9fceb6a8a636bba6374ca3d71f1e6
FantzyTHT <robertucciryan@gmail.com>
2025-08-13 20:32:56 -0400

    Create discord.js

---
+++ discord.js
@@ -0,0 +1,11 @@
+await message.reply({
+  embeds: [{
+    title: "FantzySMP",
+    url: "https://www.fantzysmp.xyz",
+    description: "FantzySMP is a growing Minecraft server offering Survival, Creative, and more exciting modes coming soon.",
+    image: {
+      url: "https://www.fantzysmp.xyz/server_banner.png"
+    },
+    color: "#ffffff"
+  }]
+});
